5ad40b4e57088c1a2cb17a764488767a4a365149
Karl Kraemer <karlekraemer@gmail.com>
2022-11-01 17:16:17 -0500

    spent a lot of time on this yesterday but we could not figure it out so we will try again tomorrow night

--- client.js
+++ client.js
@@ -45,13 +45,48 @@ console.log('array of employee data: ',  employees );
 
 
 
-
-// This function will calculate 1 employee's bonus!
-//
-function calculateIndividualEmployeeBonus( employee ) {  
+// This function will calculate 1 employee's bonus
   // your logic here
-  
-  
-  // return new object with bonus results
+  // run through the objects, take in one employeee object and return a new one with the following
+  // name, bonus percentage, total compensation (base anual + bonus), and total bonus
+  // individual bonus rules: rating 2 or below = no bonus; 3 = 4% of base; 4 = 6% of base; 5 = 10% of base
+    // OBJECTIVE 1 ^^^
 
-}
+  // if employee number is 4 digits long = additional 5%
+  // if annual is > 65k = bonus adjusted -1%
+  // no bonus below 0% or above 13%
+  
+
+  // Write a declared function that takes in one **Employee** object (as an argument to the function), and `return` a new **object** with the following properties. _Note these properties are different than the ones you start with!_
+
+  // * The `name` property should contain the employee's name.
+  // * The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+  // * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+  // * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+
+let employee = employees.name;
+
+
+  
+function calculateIndividualEmployeeBonus(employee) {  
+    // your logic here
+  let bonusPercentage = 0;
+    for (let i=0; i < employees.length; i++) {
+      if (employees[i].reviewRating <= 2) {
+         employees.bonusPercentage + 0
+        }
+      if (employees[i].reviewRating = 3) {
+         employees.bonusPercentage + .04
+        }
+      if (employees[i].reviewRating = 4) {
+         employees.bonusPercentage + .06
+        }
+      if (employees[i].reviewRating = 5) {
+         employees.bonusPercentage + .1
+        }
+    }
+  return bonusPercentage
+    // return new object with bonus results
+  
+  }
+  console.log(calculateIndividualEmployeeBonus('Jem'));
